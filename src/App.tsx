a6ec7b625bf959c8ac60fba6514d477005451852
Natalie Harrison <harrisonNatalie04@outlook.com>
2023-01-19 13:30:26 -0500

    adding dividers and updating grid size

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import Grid from '@mui/material/Grid';
+import Divider from '@mui/material/Divider';
 import './App.css';
 import Header from './components/header';
 import AboutMe from './components/aboutme';
 import Resume from './components/resume';
 import Tools from './components/tools';
 
+
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Grid container spacing = {2}>
+      <Grid container spacing = {2} sx = {{mt: '5px'}}>
 
-        <Grid item xs = {4} md = {4}> 
+        <Grid item xs = {3} md = {3}> 
         <AboutMe></AboutMe>
         </Grid> 
+        <Divider orientation="vertical" flexItem ></Divider>
 
-        <Grid item xs = {4} md = {4}> 
+        <Grid item xs = {5} md = {5}> 
         <Resume></Resume>
         </Grid> 
+        <Divider orientation="vertical" flexItem ></Divider>
 
-        <Grid item xs = {4} md = {4}> 
+        <Grid item xs = {3} md = {3}> 
         <Tools></Tools>
         </Grid> 
 
