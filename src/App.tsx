ee51dfde711acbac67da85a11d960a398b34f2db
Natalie Harrison <harrisonNatalie04@outlook.com>
2023-01-19 12:45:02 -0500

    adding grid for 3 sections
    1) about me
    2) resume
    3) tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import Grid from '@mui/material/Grid';
 import './App.css';
 import Header from './components/header';
 
@@ -7,6 +8,18 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Grid container spacing = {2}>
+
+        <Grid item xs = {4} md = {4}> 
+        </Grid> 
+
+        <Grid item xs = {4} md = {4}> 
+        </Grid> 
+
+        <Grid item xs = {4} md = {4}> 
+        </Grid> 
+
+      </Grid>
     </div>
   );
 }
