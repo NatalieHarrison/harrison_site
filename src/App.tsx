884bdf9f411cee4f6d88ec54f0e90d9195685842
Natalie Harrison <harrisonNatalie04@outlook.com>
2023-02-27 14:53:16 -0500

    mobile fix attempt 1

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
         </Grid> 
         <Divider orientation="vertical" flexItem ></Divider>
 
-        <Grid item xs = {8} md = {7} style={{maxHeight: 700, overflow: 'auto'}}> 
+        <Grid item xs = {7} md = {7} style={{maxHeight: 700, overflow: 'auto'}}> 
           <Resume></Resume>
         </Grid>
 
