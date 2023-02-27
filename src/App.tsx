42f0f1bb6fd5893116200572ff79b5e670006ad2
Natalie Harrison <harrisonNatalie04@outlook.com>
2023-02-27 14:59:14 -0500

    Mobile fix attempt 2

--- src/App.tsx
+++ src/App.tsx
@@ -15,12 +15,12 @@ function App() {
       <Header></Header>
       <Grid container spacing = {2} sx = {{mt: '5px'}}>
 
-        <Grid item xs = {4} md = {4} style={{maxHeight: 700, overflow: 'auto'}}> 
+        <Grid item xs = {6} md = {4} style={{maxHeight: 700, overflow: 'auto'}}> 
           <AboutMe></AboutMe>
         </Grid> 
         <Divider orientation="vertical" flexItem ></Divider>
 
-        <Grid item xs = {7} md = {7} style={{maxHeight: 700, overflow: 'auto'}}> 
+        <Grid item xs = {6} md = {7} style={{maxHeight: 700, overflow: 'auto'}}> 
           <Resume></Resume>
         </Grid>
 
