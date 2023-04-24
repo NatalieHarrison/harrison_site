68a23774cd3da543957e4333c17395814ccceeed
Natalie Harrison <harrisonNatalie04@outlook.com>
2023-04-24 12:11:35 -0400

    appearance, text field and button

--- src/components/timer/timerTool.tsx
+++ src/components/timer/timerTool.tsx
@@ -1,8 +1,15 @@
+import { Button, TextField } from '@mui/material';
 import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 function TimerTool(){
+
   return(
     <Box>
-      
+      <Stack direction = "row" spacing={2} >
+      <TextField id="outlined-basic" label="Hours" variant="outlined" sx = {{width: '60px'}}/>
+      <TextField id="outlined-basic" label="Mins" variant="outlined" sx = {{width: '60px'}}/>
+      <Button variant="contained">Start Timer</Button>
+      </Stack>
     </Box>
   )
 }
