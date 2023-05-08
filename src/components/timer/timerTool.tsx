47298cac0e9b508f3b5fd45780b925dc42ba33fb
Natalie Harrison <harrisonNatalie04@outlook.com>
2023-05-08 12:59:30 -0400

    Revert "Revert "user input (with flaws)""
    This reverts commit bd27ef53c1717f2a8f2a7ea4ce14cb0f6ba4a56a.

--- src/components/timer/timerTool.tsx
+++ src/components/timer/timerTool.tsx
@@ -6,20 +6,35 @@ import CountDownTimer from './countDownTimer';
 
 const TimerTool = () => {
 
-  const [hrsInput, setHrsInput] = useState(0);
-  const [minsInput, setMinsInput] = useState(0);
-  
+  let [hrsInput, setHrsInput] = useState("");
+  let [minsInput, setMinsInput] = useState("");
+  let [startTimer, setTimer] = useState(false);
 
-
+  const startTimerClick = () => {
+    setTimer(current => !current)
+  }
   return(
     <Box>
 
 
       <Stack direction = "row" spacing={2} >
-      <TextField id="outlined-basic" label="Hours" variant="outlined" sx = {{width: '60px'}} value={hrsInput} />
-      <TextField id="outlined-basic" label="Mins" variant="outlined" sx = {{width: '60px'}} value={minsInput} />
-      <Button variant="contained">Start Timer</Button>
-      <CountDownTimer hours = {hrsInput} minutes = {minsInput} seconds = {0}></CountDownTimer>
+
+      <TextField id="outlined-basic" label="Hours" variant="outlined" sx = {{width: '60px'}} value={hrsInput} onChange={(e) => setHrsInput(e.target.value)}/>
+      <TextField id="outlined-basic" label="Mins" variant="outlined" sx = {{width: '60px'}} value={minsInput} onChange={(e) => setMinsInput(e.target.value)} />
+
+
+      <Button
+       variant="contained" 
+       onClick = {startTimerClick}>
+        Start Timer
+      </Button>
+      {startTimer && (
+        <div>
+          <CountDownTimer hours = {parseInt(hrsInput)} minutes = {parseInt(minsInput)} seconds = {0}></CountDownTimer>
+        </div>
+      )}
+      {/* <CountDownTimer hours = {parseInt(hrsInput)} minutes = {parseInt(minsInput)} seconds = {0}></CountDownTimer> */}
+
       </Stack>
     </Box>
   )
