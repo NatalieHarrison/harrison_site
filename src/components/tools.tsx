16d1bccdf7b7fa1f5732c94737a73dd29f46d048
Natalie Harrison <harrisonNatalie04@outlook.com>
2023-05-09 12:34:02 -0400

    cleaning up tools page

--- src/components/tools.tsx
+++ src/components/tools.tsx
@@ -1,16 +1,33 @@
-import { Box, Typography} from "@mui/material";
+import { Box, Stack, Typography} from "@mui/material";
 import TimerTool from "./timer/timerTool";
+import DisplayCalculator from "./mathComponents/displayCalculator";
 
 function Tools() {  
   return (
     <div className="App">
 
       <Box sx = {{ml: '20px'}}>
-      <Typography variant = "h5" component="div" sx ={{flexGrow:1}}>
+        <Typography variant = "h5" component="div" sx ={{flexGrow:1}}>
             Tools
-          </Typography>
+        </Typography>
 
-            <TimerTool ></TimerTool>
+        <Stack direction = "row" spacing ={10}>
+
+          <Stack direction = "column">
+            <Typography variant = "h6">
+              Calculator
+            </Typography>
+            <DisplayCalculator/>
+          </Stack>
+
+          <Stack direction = "column" spacing = {2}>
+            <Typography variant = "h6">
+              Countdown Timer
+            </Typography>
+            <TimerTool/>
+          </Stack>
+          
+        </Stack>
       </Box>
     </div>
   );
