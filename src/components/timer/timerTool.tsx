8c976fd839dca70f0784c75740d056f75988c68b
Natalie Harrison <harrisonNatalie04@outlook.com>
2023-05-12 16:24:19 -0400

    layout adjustments
    -hiding text fields when set/reset timer button is clicked
    -set/reset button is moved to under text fields and timer

--- src/components/timer/timerTool.tsx
+++ src/components/timer/timerTool.tsx
@@ -8,33 +8,39 @@ const TimerTool = () => {
   let [hrsInput, setHrsInput] = useState("");
   let [minsInput, setMinsInput] = useState("");
   let [startTimer, setTimer] = useState(false);
+  let [buttonHide, setButtonHide] = useState(true);
 
 
 
   const startTimerClick = () => {
     setTimer(current => !current) //displays the timer and hides it when button clicks 
     // setResetTimer (current => !current)
+    setButtonHide(current => !current)
   }
 
   return(
     <Box>
-      <Stack direction = "row" spacing={2} >
+      <Stack spacing = {1}>
+        {buttonHide &&( //hiding hours and mins textfields when timer starts
+          <div>
+            <Stack direction = "row" spacing={1} justifyContent="center">
+            <TextField id="outlined-number" label="Hours" InputLabelProps={{shrink: true, }} variant="outlined" sx = {{width: '60px'}} value={hrsInput} onChange={(e) => setHrsInput(e.target.value)}/>
+            <TextField id="outlined-number" label="Mins"  InputLabelProps={{shrink: true,}} variant="outlined" sx = {{width: '60px'}} value={minsInput} onChange={(e) => setMinsInput(e.target.value)} />
+            </Stack>
+          </div>
+        )}
 
-      <TextField id="outlined-number" label="Hours" InputLabelProps={{shrink: true, }} variant="outlined" sx = {{width: '60px'}} value={hrsInput} onChange={(e) => setHrsInput(e.target.value)}/>
-      <TextField id="outlined-number" label="Mins"  InputLabelProps={{shrink: true,}} variant="outlined" sx = {{width: '60px'}} value={minsInput} onChange={(e) => setMinsInput(e.target.value)} />
+        {startTimer && ( //hides timer until "set/reset timer" button is clicked
+          <div>
+            <CountDownTimer hours = {parseInt(hrsInput)} minutes = {parseInt(minsInput)} seconds = {0}></CountDownTimer>
+          </div>
+        )}
 
-      <Button
-       variant="contained" 
-       onClick = {startTimerClick}>
-        Set/Reset Timer
-      </Button>
-
-      {startTimer && (
-        <div>
-          <CountDownTimer hours = {parseInt(hrsInput)} minutes = {parseInt(minsInput)} seconds = {0}></CountDownTimer>
-        </div>
-      )}
-
+        <Button
+        variant="contained" 
+        onClick = {startTimerClick}>
+          Set/Reset Timer
+        </Button>
 
       </Stack>
     </Box>
