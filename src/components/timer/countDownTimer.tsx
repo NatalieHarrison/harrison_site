29ed421d0d5be30a8f1b63082fbea09da57b1b9f
Natalie Harrison <harrisonNatalie04@outlook.com>
2023-05-12 16:00:43 -0400

    Timer fixed

--- src/components/timer/countDownTimer.tsx
+++ src/components/timer/countDownTimer.tsx
@@ -15,11 +15,15 @@ const CountDownTimer = ({ hours = 0, minutes = 0, seconds = 60}:ICountdown) => {
   const tick = () => {
     if (time.hours === 0 && time.minutes === 0 && time.seconds === 0)
       reset()
-    // else if (time.hours === 0 && time.seconds === 0){
-    //   setTime({hours: time.hours - 1, minutes:59, seconds: 59});}
-     else if (time.seconds === 0){
-      setTime({hours: time.hours, minutes: time.minutes-1, seconds:59});
-    } else {
+    else if (time.hours === 0 && time.seconds === 0){
+      setTime({hours: time.hours, minutes: time.minutes-1, seconds: 59});}
+    else if (time.minutes ===0 && time.seconds === 0){
+      setTime({hours: time.hours-1, minutes: 59, seconds:59});
+    } 
+    else if(time.hours !== 0 && time.minutes !== 0 && time.seconds ===0){
+      setTime({hours: time.hours-1, minutes:59, seconds: 59 })
+    }
+    else {
       setTime({hours: time.hours, minutes: time.minutes, seconds: time.seconds - 1 })
     }
   }
