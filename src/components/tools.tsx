0f1a76a4ef8dd732824c77b34b24b538f6ba7aea
Natalie Harrison <harrisonNatalie04@outlook.com>
2023-05-22 17:14:37 -0400

    updating header for tools

--- src/components/tools.tsx
+++ src/components/tools.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Typography} from "@mui/material";
+import { Box, Container, Stack, Typography} from "@mui/material";
 import TimerTool from "./timer/timerTool";
 import DisplayCalculator from "./mathComponents/displayCalculator";
 
@@ -7,9 +7,11 @@ function Tools() {
     <div className="App">
 
       <Box ml = '20px' mb = '150px' mt = '20px' >
-        <Typography variant = "h5" component="div" sx ={{flexGrow:1}}>
-            Tools
-        </Typography>
+        <Container sx = {{backgroundColor: '#ED6A5A',p:1, borderRadius: 1}}>
+          <Typography variant = 'h4' align='center' sx = {{color: 'white'}}>
+              Tools
+          </Typography>
+        </Container>
 
         <Stack direction = "row" spacing ={10}>
 
