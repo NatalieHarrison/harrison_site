8b0d035e71220269594709563e5cf30a122f4a02
Natalie Harrison <harrisonNatalie04@outlook.com>
2023-05-22 16:55:05 -0400

    "2023 resume" changes
    Added color and made header bigger

--- src/components/resume.tsx
+++ src/components/resume.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import { Container, Typography } from '@mui/material';
 import Education from './resumeComponents/education';
 import Skills from './resumeComponents/skills';
 import Projects from './resumeComponents/projects';
@@ -9,9 +9,13 @@ import Involvement from './resumeComponents/involvement';
 function Resume (){
   return(
     <Box mb= "110px">
-       <Typography variant = "h5" component="div" sx ={{flexGrow:1}}>
+      <Container sx = {{backgroundColor: '#ED6A5A', mt: '20px', mb: '20px',p:1, borderRadius: 1}}>
+      <Typography variant = 'h4' align='center' sx = {{color: 'white'}}>
             2023 Resume
           </Typography>
+
+      </Container>
+      
           <Education></Education>
           <Skills></Skills>
           <Projects></Projects>
