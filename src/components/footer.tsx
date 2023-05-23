cf5765a98e5a17f530e3b4fb1321034432d14b23
Natalie Harrison <harrisonNatalie04@outlook.com>
2023-05-23 15:41:20 -0400

    removing footer, adding to navBar

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,33 +2,33 @@ import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
 function Footer(){
 
   return(
-    <Box sx = {{ backgroundColor: '#1C3879', height: '100px', width: '100%'}} position={'fixed'} bottom={0} >
-        <Typography variant = "h5" align = 'center' sx = {{color: '#F9F5EB'}}>
+    <Box sx = {{ backgroundColor: '#55C5D1', height: '100px', width: '100%'}} position={'fixed'} bottom={0} >
+        <Typography variant = "h5" align = 'center' sx = {{color: '#F9F5EB', mt: '5px'}}>
             Links
         </Typography>
       <Stack  direction="row" justifyContent="space-evenly" alignItems="center" spacing={2}>
-        <Button variant = "contained" href='https://github.com/NatalieHarrison' sx = {{textTransform: 'none', backgroundColor: '#607EAA' }}>
-          <Typography sx = {{color: '#F9F5EB'}}>
-           GitHub
-          </Typography>
+        <IconButton  href='https://github.com/NatalieHarrison' sx = {{textTransform: 'none', backgroundColor: '#4699C2' }}>
+          <GitHubIcon/>
         
-        </Button>
-        <Button variant = "contained" href="https://www.linkedin.com/in/natalieharrison45"  sx = {{textTransform: 'none', backgroundColor: '#607EAA'  }}>
-          <Typography sx = {{color: '#F9F5EB'}}>
-            LinkedIn
-          </Typography>
-        </Button>
+        </IconButton>
+        <IconButton  href="https://www.linkedin.com/in/natalieharrison45"  sx = {{textTransform: 'none', backgroundColor: '#4699C2'  }}>
+          <LinkedInIcon/>
+        </IconButton>
 
-        <Button variant = "contained" href="https://ksuid.net"  sx = {{textTransform: 'none', backgroundColor: '#607EAA'  }}>
+        <Button href="https://ksuid.net"  sx = {{textTransform: 'none', backgroundColor: '#4699C2'  }}>
           <Typography sx = {{color: '#F9F5EB'}}>
             KSuid
           </Typography>
         </Button>
 
-        <Button variant = "contained" href="https://www.headstogetherhealth.com"  sx = {{textTransform: 'none', backgroundColor: '#607EAA'  }}>
+        <Button  href="https://www.headstogetherhealth.com"  sx = {{textTransform: 'none', backgroundColor: '#4699C2'  }}>
           <Typography sx = {{color: '#F9F5EB'}}>
             Heads Together Health
           </Typography>
--- src/components/navBar/Navbar.tsx
+++ src/components/navBar/Navbar.tsx
@@ -1,7 +1,11 @@
-import { Box } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import Appbar from '@mui/material/AppBar';
 import Typography from '@mui/material/Typography';
 import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import { Stack } from "@mui/system";
@@ -10,51 +14,85 @@ const Navbar = () => {
   return (
   <Box >
       <Appbar position="static">
-        <Toolbar sx ={{backgroundColor: '#1C3879'}}>
-          <Typography variant = "h6" component="div" sx ={{ color: '#F9F5EB'}}>
-            Natalie Harrison
-          </Typography>
-          
-            <ul style={{listStyle: 'none'}}>
-            <Stack direction = "row" spacing = {3}>
-              <li>
-                <Link to= "/">
-                  <Typography variant = "subtitle1" component="div" sx ={{ color: '#F9F5EB'}}>
-                  Home
-                  </Typography>
-                  </Link>
-              </li>
+      <Grid>
+        <Toolbar sx ={{backgroundColor: '#55C5D1'}}>
+            <Grid xs = {2} md = {2}>
+            <Typography variant = "h6" component="div" sx ={{ color: '#F9F5EB'}}>
+              Natalie Harrison
+            </Typography>
+            </Grid>
 
-              <li>
-                <Link to= "/about">
+            <Grid xs = {4} md = {4}>
+              <ul style={{listStyle: 'none'}}>
+              <Stack direction = "row" spacing = {3}>
+                <li>
+                  <Link to= "/" style={{ textDecoration: 'none' }}>
+                    <Typography variant = "subtitle1" component="div" sx ={{ color: '#F9F5EB'}}>
+                    Home
+                    </Typography>
+                    </Link>
+                </li>
+
+                <li>
+                  <Link to= "/about" style={{ textDecoration: 'none' }}>
+                    
+                  <Typography variant = "subtitle1" component="div" sx ={{ color: '#F9F5EB'}}>
+                    About
+                  </Typography>
                   
-                <Typography variant = "subtitle1" component="div" sx ={{ color: '#F9F5EB'}}>
-                  About
-                </Typography>
+                  </Link>
+                </li>
+
+                <li>
+                  <Link to= "/resume" style={{ textDecoration: 'none' }}>
+                    <Typography variant = "subtitle1" component="div" sx ={{ color: '#F9F5EB'}}>
+                      Resume
+                    </Typography>
+                  </Link>
+                </li>
+
+                <li>
+                  <Link to= "/tools" style={{ textDecoration: 'none' }}>
+                    <Typography variant = "subtitle1" component="div" sx ={{ color: '#F9F5EB'}}>
+                      Tools
+                    </Typography>
+                    
+                    </Link>
+                </li>
+                </Stack>
+              </ul>
+              </Grid>
+              
+              <Grid xs = {4} md = {4}>
+              <Stack  direction="row"  alignItems="center"   spacing={3} sx = {{ml :'20px'}}>
+                <IconButton  href='https://github.com/NatalieHarrison' sx = {{textTransform: 'none', backgroundColor: '#4699C2' }}>
+                  <GitHubIcon/>
                 
-                </Link>
-              </li>
+                </IconButton>
+                <IconButton  href="https://www.linkedin.com/in/natalieharrison45"  sx = {{textTransform: 'none', backgroundColor: '#4699C2'  }}>
+                  <LinkedInIcon/>
+                </IconButton>
+      
 
-              <li>
-                <Link to= "/resume">
-                  <Typography variant = "subtitle1" component="div" sx ={{ color: '#F9F5EB'}}>
-                    Resume
-                  </Typography>
-                </Link>
-              </li>
-
-              <li>
-                <Link to= "/tools">
-                  <Typography variant = "subtitle1" component="div" sx ={{ color: '#F9F5EB'}}>
-                    Tools
-                  </Typography>
-                  
-                  </Link>
-              </li>
-              </Stack>
-            </ul>
             
+                <Typography>
+                  Personal Projects: 
+                </Typography>
+                <Button href="https://ksuid.net"  sx = {{textTransform: 'none', backgroundColor: '#4699C2'  }}>
+                  <Typography sx = {{color: '#F9F5EB'}}>
+                    KSuid
+                  </Typography>
+                </Button>
+
+                <Button  href="https://www.headstogetherhealth.com"  sx = {{textTransform: 'none', backgroundColor: '#4699C2'  }}>
+                  <Typography sx = {{color: '#F9F5EB'}}>
+                    Heads Together Health
+                  </Typography>
+                </Button>
+              </Stack>
+              </Grid>
         </Toolbar>
+        </Grid>
       </Appbar>
     </Box>
   );
