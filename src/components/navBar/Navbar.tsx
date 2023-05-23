3f9171c84f0337ece8ca70c1487441420ee1ffed
Natalie Harrison <harrisonNatalie04@outlook.com>
2023-05-23 15:55:14 -0400

    making router links into buttons
    removed li/ul and make buttons instead

--- src/components/navBar/Navbar.tsx
+++ src/components/navBar/Navbar.tsx
@@ -19,45 +19,47 @@ const Navbar = () => {
               Natalie Harrison
             </Typography>
 
-            <ul style={{listStyle: 'none'}}>
-              <Stack direction = "row" spacing = {3}>
-                <li>
+  
+              <Stack direction = "row" spacing = {2} ml = "20px">
+                <Button variant="text" sx = {{textTransform: 'none'}}>
                   <Link to= "/" style={{ textDecoration: 'none' }}>
                     <Typography variant = "subtitle1" component="div" sx ={{ color: '#F9F5EB'}}>
                     Home
                     </Typography>
                     </Link>
-                </li>
+                </Button>
+               
 
-                <li>
+                <Button variant="text" sx = {{textTransform: 'none'}}>
                   <Link to= "/about" style={{ textDecoration: 'none' }}>
                     
-                  <Typography variant = "subtitle1" component="div" sx ={{ color: '#F9F5EB'}}>
+                  <Typography variant = "subtitle1"  sx ={{ color: '#F9F5EB'}}>
                     About
                   </Typography>
                   
                   </Link>
-                </li>
+                </Button>
+            
 
-                <li>
+                <Button variant="text" sx = {{textTransform: 'none'}}>
                   <Link to= "/resume" style={{ textDecoration: 'none' }}>
-                    <Typography variant = "subtitle1" component="div" sx ={{ color: '#F9F5EB'}}>
+                    <Typography variant = "subtitle1"  sx ={{ color: '#F9F5EB'}}>
                       Resume
                     </Typography>
                   </Link>
-                </li>
+                </Button>
 
-                <li>
+                <Button variant="text" sx = {{textTransform: 'none'}}>
                   <Link to= "/tools" style={{ textDecoration: 'none' }}>
-                    <Typography variant = "subtitle1" component="div" sx ={{ color: '#F9F5EB'}}>
+                    <Typography variant = "subtitle1" sx ={{ color: '#F9F5EB'}}>
                       Tools
                     </Typography>
                     
                     </Link>
-                </li>
+                </Button>
               </Stack>
-            </ul>
-
+         
+            
             <Stack  direction="row"  alignItems="center"   spacing={3} sx = {{ml :'20px'}}>
               <IconButton  href='https://github.com/NatalieHarrison' sx = {{textTransform: 'none', backgroundColor: '#4699C2' }}>
                 <GitHubIcon/>
