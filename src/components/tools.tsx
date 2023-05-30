137e55d889f17d4ee16f821bef3dc66324892224
Natalie Harrison <harrisonNatalie04@outlook.com>
2023-05-30 14:27:41 -0400

    tools responsive

--- src/components/tools.tsx
+++ src/components/tools.tsx
@@ -15,14 +15,14 @@ function Tools() {
       <Container sx = {{backgroundColor:'white'}}>
 
         <Grid container direction = "row" >
-          <Grid xs = {6} md = {6}>
+          <Grid xs = {11} md = {6} >
             <Typography variant = "h6">
               Calculator
             </Typography>
             <DisplayCalculator/>
           </Grid>
 
-          <Grid xs = {6} md = {6} >
+          <Grid xs = {11} md = {6} sx = {{mb: '10px'}}>
             <Typography variant = "h6">
               Countdown Timer
             </Typography> 
