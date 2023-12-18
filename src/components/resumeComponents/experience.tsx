cefb386e0a190ce2e3d9c3a7857980d972fcb665
Natalie Harrison <harrisonNatalie04@outlook.com>
2023-12-18 11:56:37 -0500

    Updated work experience
    YSA internship and updated descriptions for other internships

--- src/components/resumeComponents/experience.tsx
+++ src/components/resumeComponents/experience.tsx
@@ -5,26 +5,32 @@ function Experience(){
         <Typography variant = "h6" >
             <b><u>WORK EXPERIENCE</u></b>
             <br></br>
-            <b>Software Engineer</b>, Heads Together Health | Remote | <b>June 2022 - Present </b>
+
+            <b>Website Application Developer, Intern </b>, Young Scientist Academy | Wilmington | <b>September 2023 - December 2023 </b>
+            <Typography variant = "body1">
+                <ul>
+                  <li>
+                  Developing organization website with tech stack: Python Django, Bootstrap and SQL.
+                  </li>
+                  <li>
+                   Lead technology and coding programs for elementary and middle school aged kids. 
+                  </li>
+                </ul>
+              </Typography>
+
+
+            <b>Software Engineer Intern</b>, Heads Together Health | Remote | <b>June 2022 - June 2023 </b>
           </Typography>
 
               <Typography variant = "body1">
                 <ul>
                   <li>
-                  Developed a dynamic website utilizing React, Amplify, and Material UI, enabling users to access comprehensive sports injury data.
+                  Built a dynamic React website with Amplify and Material UI, providing access to extensive sports injury data. 
                   </li>
                   <li>
-                  Collaborated with esteemed organizations, including Datalys and UNC Health, in the health and data sectors to ensure the accuracy and relevancy of the information provided. 
+                   Strong collaboration for ensuring functionality, design and UX throughout development. 
                   </li>
-                  <li>
-                  Designed and implemented the company’s website using Square Space, gaining knowledge on creating a seamless user experience and visually appealing interface.
-                  </li>
-                  <li>
-                  Demonstrated strong collaboration skills and exceptional attention to detail throughout the development process, ensuring the website’s functionality, design, and user experiences were of the highest standard.
-                  </li>
-                  <li>
-                  Position acquired through Upwork (free-lance employment platform)
-                  </li>
+              
                 </ul>
               </Typography>
 
@@ -38,13 +44,10 @@ function Experience(){
               Gained extensive knowledge in React, demonstrating a comprehensive understanding of its concepts and best practices.
               </li>
               <li>
-              Exhibited exceptional client communication skills, actively collaborating on interface designs and provide timely updates on site development progress.
-              </li>
+                Developed client communication skills, collaborated on UI designs, and provided timely progress updates.
+              </li> 
               <li>
-              Demonstrated a commitment to quality assurance by ensuring continuous integration and ensuring the stability and functionality of web apps.
-              </li>
-              <li>
-              Demonstrated proficiency in Adobe XD, using it to create visually engaging and intuitive mock-ups.
+                Demonstrated proficiency in Adobe XD, using it to create visually engaging and intuitive mock-ups. 
               </li>
             </ul>
           </Typography>
