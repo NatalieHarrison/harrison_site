5b6614bd79fb6111aafa272d8e465022c5e3e2d4
Natalie Harrison <harrisonNatalie04@outlook.com>
2023-12-22 12:33:20 -0500

    Color updates

--- src/components/colors.ts
+++ src/components/colors.ts
@@ -1,6 +1,6 @@
 // colors.ts
 
-export const PRIMARY_COLOR = "#427D9D";
+export const PRIMARY_COLOR = "#5B8FB9";
 export const SECONDARY_COLOR = "#9BBEC8";
 export const ACCENT_COLOR = "#DDF2FD";
 export const BACKGROUND_COLOR = "#164863";
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,12 +3,12 @@ import { Outlet } from "react-router";
 import Navbar from "./navBar/Navbar";
 import Footer from "./footer";
 import { Box } from "@mui/material";
-import { BACKGROUND_COLOR } from "./colors";
+import { ACCENT_COLOR, BACKGROUND_COLOR } from "./colors";
 
 const Layout = () => {
   return(
     <>
-    <Box sx = {{backgroundColor: BACKGROUND_COLOR,  mt: '10px'}}>
+    <Box sx = {{backgroundColor: ACCENT_COLOR,  mt: '10px'}}>
     
       <Navbar/>
       <Outlet/>
